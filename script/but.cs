4ac96495ece225cc0b839d960ddb74c85f8d59b3
NightRoadX <40123156+JamesGombami@users.noreply.github.com>
2018-12-24 17:05:41 +0200

    Add files via upload

---
+++ script/but.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class but : MonoBehaviour {
+
+    public GameObject videoPanel;
+    private bool isVideoPlaying = false;
+    public static VideoPlayer video;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        video = videoPanel.GetComponent<VideoPlayer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void PlayVideo()
+    {
+            video.Play();
+            isVideoPlaying = true;
+    }
+
+    public void StopVideo()
+    {
+        video.Stop();
+        isVideoPlaying = false;
+    }
+
+
+    public void OnButtonReleased()
+    {
+
+    }
+}
---
+++ script/gameX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameX : MonoBehaviour
+{
+    public GameObject HSpanel;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = HSpanel.GetComponent<Animator>();
+    }
+
+    public void changeScene(string name)
+    {
+        SceneManager.LoadSceneAsync(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void hideObject(string name)
+    {
+        GameObject.Find(name).SetActive(false);
+
+    }
+    public void playHSpanel()
+    {
+        anim.SetTrigger("play");
+    }
+    public void backHSpanel()
+    {
+        anim.SetTrigger("Stop");
+    }
+}
